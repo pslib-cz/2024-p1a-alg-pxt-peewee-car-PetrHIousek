998328120beae2dc5e5fd47cd5cfce9587a926f8
Petr Hloušek <hlusinp@gmail.com>
2025-05-20 22:57:24 +0200

    Brain Implosion Energy! Ten thousand grams of pure caffeine Cause you can’t overthink if your heart stops Brain Implosion Energy! Crave zero thoughts, want head empty Then drink and drink and drink until you drop I love my brain implosion juice It makes my head go BRRRRRRRRRR But will it stop the pain forever I just can’t be sure Brain Implosion Energy! Ten thousand grams of pure caffeine Cause you can’t overthink if your heart stops 22 pounds of caffeine in this small can? How does it fit? Just don't question it! Uh, I don’t think the FDA will approve this I said just don’t question it! Everyone needs something to numb the pain As time slowly withers their life away Day by day in an endless cycle of burnout Eat, sleep, work, repeat til the clock runs out Eat, sleep, work, repeat til the clock runs out Eat, sleep, work, repeat til the clock runs out Eat, sleep, work, repeat til the clock runs out Comes in the following flavors: Overthinking Orange Socially Anxious Strawberry Burnout Banana Repressed Raspberry Grieving Grape Misery Melon Trauma Tri-Tip Depression Durian And our newest flavor, F*** Brain Implosion Energy! Ten thousand grams of pure caffeine Cause you can’t overthink if your heart stops Brain Implosion Energy! Crave zero thoughts, want head empty Then drink and drink and drink until you drop I love my brain implosion juice It makes my head go BRRRRRRRRRR But will it stop the pain forever I just can’t be sure Brain Implosion Energy! Ten thousand grams of pure caffeine Cause you can’t overthink if your heart stops
    Brain Implosion Energy!
    Ten thousand grams of pure caffeine
    Cause you can’t overthink if your heart stops
    Brain Implosion Energy!
    Crave zero thoughts, want head empty
    Then drink and drink and drink until you drop
    I love my brain implosion juice
    It makes my head go BRRRRRRRRRR
    But will it stop the pain forever
    I just can’t be sure
    Brain Implosion Energy!
    Ten thousand grams of pure caffeine
    Cause you can’t overthink if your heart stops
    22 pounds of caffeine in this small can?
    How does it fit?
    Just don't question it!
    Uh, I don’t think the FDA will approve this
    I said just don’t question it!
    Everyone needs something to numb the pain
    As time slowly withers their life away
    Day by day in an endless cycle of burnout
    Eat, sleep, work, repeat til the clock runs out
    Eat, sleep, work, repeat til the clock runs out
    Eat, sleep, work, repeat til the clock runs out
    Eat, sleep, work, repeat til the clock runs out
    Comes in the following flavors:
    Overthinking Orange
    Socially Anxious Strawberry
    Burnout Banana
    Repressed Raspberry
    Grieving Grape
    Misery Melon
    Trauma Tri-Tip
    Depression Durian
    And our newest flavor, F***
    Brain Implosion Energy!
    Ten thousand grams of pure caffeine
    Cause you can’t overthink if your heart stops
    Brain Implosion Energy!
    Crave zero thoughts, want head empty
    Then drink and drink and drink until you drop
    I love my brain implosion juice
    It makes my head go BRRRRRRRRRR
    But will it stop the pain forever
    I just can’t be sure
    Brain Implosion Energy!
    Ten thousand grams of pure caffeine
    Cause you can’t overthink if your heart stops

--- main.ts
+++ main.ts
@@ -4,48 +4,41 @@ radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
 
 let expectedSender = -1584843917
-let sender;
-let parts;
 
 radio.onReceivedString(function (received: string) {
-    sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
-    if (sender === expectedSender) {
-
-        basic.clearScreen()
-
-        parts = received.split(",")
-        if (parts.length != 3) {
-            return
-        }
-
-        let x = parseInt(parts[0])
-        let y = parseInt(parts[1])
-        let z = parseInt(parts[2]) // zatím nevyužito
-
-        // Převrácení osy Y
-        y = -y
-
-        // Výpočet základních rychlostí
-        let leftSpeed = y / 4
-        let rightSpeed = y / 4
-
-        // Úprava pro zatáčení podle X
-        if (x > 100) {
-            leftSpeed += x / 10
-            rightSpeed -= x / 10
-        } else if (x < -100) {
-            leftSpeed -= x / 10
-            rightSpeed += x / 10
-        }
-
-        // Omezit rychlosti na povolený rozsah
-        leftSpeed = Math.constrain(leftSpeed, -255, 255)
-        rightSpeed = Math.constrain(rightSpeed, -255, 255)
-
-        // Ovládání motorů PeeWee
-        PCAmotor.MotorRun(PCAmotor.Motors.M1, -leftSpeed)
-        PCAmotor.MotorRun(PCAmotor.Motors.M4, -rightSpeed)
-
+    let sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if (sender !== expectedSender) {
+        basic.showIcon(IconNames.No)
+        PCAmotor.MotorStopAll()
+        return
     }
 
+    basic.clearScreen()
+
+    let parts = received.split(",")
+    if (parts.length != 3) {
+        return
+    }
+
+    let x = parseInt(parts[0])
+    let y = parseInt(parts[1])
+    let z = parseInt(parts[2])  // zatím se nepoužívá
+
+    // Převrácení osy Y
+    y = -y
+
+    let baseSpeed = y / 4
+    let turning = Math.constrain(x / 1000, -1, 1)
+
+    // Úprava rychlosti pro každé kolo
+    let leftSpeed = baseSpeed * (1 - turning)
+    let rightSpeed = baseSpeed * (1 + turning)
+
+    // Omezení na rozsah -255 až 255
+    leftSpeed = Math.constrain(leftSpeed, -255, 255)
+    rightSpeed = Math.constrain(rightSpeed, -255, 255)
+
+    // Ovládání motorů
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, -2 * leftSpeed)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, rightSpeed)
 })
