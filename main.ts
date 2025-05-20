c4fc77e455db3b2f2ce983c8bfdd9d2c1a538fd2
Petr Hloušek <hlusinp@gmail.com>
2025-05-20 23:35:46 +0200

    Remade code, functional riding to forwards and reverse. Partially functional steering.
    Brain Implosion Energy! Ten thousand grams of pure caffeine Cause you can’t overthink if your heart stops Brain Implosion Energy! Crave zero thoughts, want head empty Then drink and drink and drink until you drop I love my brain implosion juice It makes my head go BRRRRRRRRRR But will it stop the pain forever I just can’t be sure Brain Implosion Energy! Ten thousand grams of pure caffeine Cause you can’t overthink if your heart stops 22 pounds of caffeine in this small can? How does it fit? Just don't question it! Uh, I don’t think the FDA will approve this I said just don’t question it! Everyone needs something to numb the pain As time slowly withers their life away Day by day in an endless cycle of burnout Eat, sleep, work, repeat til the clock runs out Eat, sleep, work, repeat til the clock runs out Eat, sleep, work, repeat til the clock runs out Eat, sleep, work, repeat til the clock runs out Comes in the following flavors: Overthinking Orange Socially Anxious Strawberry Burnout Banana Repressed Raspberry

--- main.ts
+++ main.ts
@@ -2,28 +2,22 @@ radio.setGroup(12)
 radio.setFrequencyBand(39)
 radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
-
 let expectedSender = -1584843917
-
 radio.onReceivedString(function (received: string) {
     let sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
-    if (sender != expectedSender) {
+    if (sender !== expectedSender) {
         basic.showIcon(IconNames.No)
         PCAmotor.MotorStopAll()
         return
     }
-
     basic.clearScreen()
-
     let parts = received.split(",")
     if (parts.length != 3) {
         return
     }
-
     let x = parseInt(parts[0])
     let y = parseInt(parts[1])
-    let z = parseInt(parts[2]) // zatím nevyužito
-
+    let z = parseInt(parts[2])  // zatím se nepoužívá
     // Převrácení osy Y
     y = -y
 
@@ -34,10 +28,10 @@ radio.onReceivedString(function (received: string) {
     // Úprava pro zatáčení podle X
     if (x > 100) {
         leftSpeed += x / 10
-        rightSpeed -= x / 10
+        rightSpeed -= x / 1
     } else if (x < -100) {
         leftSpeed += x / 10
-        rightSpeed -= x / 10
+        rightSpeed -= x / 1
     }
 
     // Omezit rychlosti na povolený rozsah
@@ -45,6 +39,6 @@ radio.onReceivedString(function (received: string) {
     rightSpeed = Math.constrain(rightSpeed, -255, 255)
 
     // Ovládání motorů PeeWee
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, -2 * leftSpeed)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, rightSpeed)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, -leftSpeed)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -rightSpeed)
 })
