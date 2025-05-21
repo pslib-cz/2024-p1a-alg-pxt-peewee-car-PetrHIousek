ace414042fc7d50d8b972709ba5a6401b670caf4
Petr Hloušek <hlusinp@gmail.com>
2025-05-21 11:26:14 +0200

    skibidi

--- main.ts
+++ main.ts
@@ -5,40 +5,41 @@ radio.setTransmitSerialNumber(true)
 let expectedSender = -1584843917
 radio.onReceivedString(function (received: string) {
     let sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
-    if (sender !== expectedSender) {
-        basic.showIcon(IconNames.No)
+    if (sender == expectedSender) {
+
+        basic.clearScreen()
+        let parts = received.split(",")
+        if (parts.length != 3) {
+            return
+        }
+        let x = parseInt(parts[0])
+        let y = parseInt(parts[1])
+        let z = parseInt(parts[2])  // zatím se nepoužívá
+        // Převrácení osy Y
+        y = -y
+
+        // Výpočet základních rychlostí
+        let leftSpeed = y / 4
+        let rightSpeed = y / 4
+
+        // Úprava pro zatáčení podle X
+        if (x > 100) {
+            leftSpeed += x / 10
+            rightSpeed -= x / 1
+        } else if (x < -100) {
+            leftSpeed += x / 10
+            rightSpeed -= x / 1
+        }
+
+        // Ovládání motorů PeeWee
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -leftSpeed)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, -rightSpeed)
+    }
+})
+
+radio.onReceivedNumber(function (onReceivedNumber: number) {
+    if (onReceivedNumber === 9989) {
+        basic.pause(500)
         PCAmotor.MotorStopAll()
-        return
     }
-    basic.clearScreen()
-    let parts = received.split(",")
-    if (parts.length != 3) {
-        return
-    }
-    let x = parseInt(parts[0])
-    let y = parseInt(parts[1])
-    let z = parseInt(parts[2])  // zatím se nepoužívá
-    // Převrácení osy Y
-    y = -y
-
-    // Výpočet základních rychlostí
-    let leftSpeed = y / 4
-    let rightSpeed = y / 4
-
-    // Úprava pro zatáčení podle X
-    if (x > 100) {
-        leftSpeed += x / 10
-        rightSpeed -= x / 1
-    } else if (x < -100) {
-        leftSpeed += x / 10
-        rightSpeed -= x / 1
-    }
-
-    // Omezit rychlosti na povolený rozsah
-    leftSpeed = Math.constrain(leftSpeed, -255, 255)
-    rightSpeed = Math.constrain(rightSpeed, -255, 255)
-
-    // Ovládání motorů PeeWee
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, -leftSpeed)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, -rightSpeed)
 })
