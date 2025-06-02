32cf2d2807a89f8df0c984e25cb390c8d3dad42e
Roman Sedlář <roman.sedlar.024@pslib.cz>
2025-06-02 10:04:48 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -10,22 +10,43 @@ type drivingSignal = {
     z: number
 }
 type lightDirection = {
+    c: DigitalPin;
+    r: DigitalPin;
+    l: DigitalPin
+}
+type data = {
     c: number;
     r: number;
     l: number
 }
 
+const IR: lightDirection = {
+    c: DigitalPin.P15,
+    r: DigitalPin.P13,
+    l: DigitalPin.P14
+}
 
 let expectedSender = 599237509;
 let ready: boolean;
-let drivingPackage: drivingSignal
-let leftSpeed: number
-let rightSpeed: number
-let parts
-let sender
+let drivingPackage: drivingSignal;
+let leftSpeed: number;
+let rightSpeed: number;
+let parts;
+let sender;
+let dataPack: data;
 
 pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
 pins.digitalReadPin(DigitalPin.P8)
+pins.setPull(IR.c, PinPullMode.PullNone)
+pins.setPull(IR.r, PinPullMode.PullNone)
+pins.setPull(IR.l, PinPullMode.PullNone)
+
+basic.forever(function () {
+    dataPack.c = pins.digitalReadPin(IR.c)
+    dataPack.r = pins.digitalReadPin(IR.r)
+    dataPack.l = pins.digitalReadPin(IR.l)
+    basic.pause(20)
+})
 
 radio.onReceivedString(function (received: string) {
     sender = radio.receivedPacket(RadioPacketProperty.SerialNumber)
@@ -36,7 +57,7 @@ radio.onReceivedString(function (received: string) {
             PCAmotor.MotorStopAll()
             basic.showString("G", 0)
         } else ready = true
-        if(ready) {
+        if (ready) {
             basic.clearScreen()
             parts = received.split(",")
             if (parts.length != 3) {
